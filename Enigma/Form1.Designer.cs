9921fac9a1e973a2ab265c1418f656a89f21bca4
Ivan Krylov <ordaswingson@gmail.com>
2018-11-30 17:34:56 +0300

    -рабочая версия без потоков и оптимизации

--- Enigma/Form1.Designer.cs
+++ Enigma/Form1.Designer.cs
@@ -28,12 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxInfo = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.buttonPath = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Шифровать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxInfo
+            // 
+            this.textBoxInfo.Location = new System.Drawing.Point(12, 74);
+            this.textBoxInfo.Multiline = true;
+            this.textBoxInfo.Name = "textBoxInfo";
+            this.textBoxInfo.ReadOnly = true;
+            this.textBoxInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxInfo.Size = new System.Drawing.Size(260, 124);
+            this.textBoxInfo.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(197, 215);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Дешифровка";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(12, 16);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(193, 20);
+            this.textBoxPath.TabIndex = 3;
+            // 
+            // buttonPath
+            // 
+            this.buttonPath.Location = new System.Drawing.Point(211, 12);
+            this.buttonPath.Name = "buttonPath";
+            this.buttonPath.Size = new System.Drawing.Size(61, 25);
+            this.buttonPath.TabIndex = 4;
+            this.buttonPath.Text = "Обзор";
+            this.buttonPath.UseVisualStyleBackColor = true;
+            this.buttonPath.Click += new System.EventHandler(this.buttonPath_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 248);
+            this.Controls.Add(this.buttonPath);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBoxInfo);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxInfo;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Button buttonPath;
     }
 }
 
--- Enigma/Form1.cs
+++ Enigma/Form1.cs
@@ -1,20 +1,189 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Enigma
 {
     public partial class Form1 : Form
     {
+        string[] files;
+        int countOfSymbol = 14;
+        string extension;//расширение файла
+
+
         public Form1()
         {
             InitializeComponent();
+            textBoxPath.Text = @"C:\Games\1";
+
+
+        }
+
+        //шифрование
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //блок проверки на пустую строку
+            if (textBoxPath.Text != "")
+            {
+                try
+                {
+                    files = Directory.GetFiles(textBoxPath.Text);
+                }         
+                catch
+                {
+                    MessageBox.Show("Папка не существует.");
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пустой путь.");
+                return;
+            }
+
+            
+            foreach (var file in files)
+            {
+                extension = file.Substring(file.LastIndexOf(".") + 1);
+
+                if (extension == "efm" || extension == "efo")
+                    continue;
+
+                int symb = new int();
+                if (extension == "mpg")
+                {
+                    
+                    
+                    using (FileStream fs = new FileStream(file, FileMode.Open))
+                    {
+                        for (var i = 0; i < fs.Length; i++)
+                        {
+                            symb = fs.ReadByte();
+                            symb++;
+                            fs.Seek(-1, SeekOrigin.Current);
+                            fs.WriteByte((byte)symb);
+                        }
+
+                        
+                    }
+
+                    string[] fName = new string[2];
+                    fName = file.Split('.');
+                    File.Move(file, fName[0] + ".efm");
+
+                }
+                else
+                {
+                    using (FileStream fs = new FileStream(file, FileMode.Open))
+                    {
+                        
+                        for (var i = 0; i < countOfSymbol; i++)
+                        {
+                            symb = fs.ReadByte();
+                            symb++;
+                            fs.Seek(-1, SeekOrigin.Current);
+                            fs.WriteByte((byte)symb);
+                        }
+                    }
+
+                    string[] fName = new string[2];
+                    fName = file.Split('.');
+                    File.Move(file, fName[0] + "=" + extension + ".efo");
+                }
+             }
+                textBoxInfo.Text = "\nКодирование завершено.";
+                
+            
+        }
+
+        //дешифровка
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBoxPath.Text != "")
+            {
+                try
+                {
+                    files = Directory.GetFiles(textBoxPath.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Папка не существует.");
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пустой путь.");
+                return;
+            }
+
+            textBoxInfo.Text = "Дешифровка начата.";
+
+            foreach (var file in files)
+            {
+                byte[] array = new byte[countOfSymbol];
+                extension = file.Substring(file.LastIndexOf(".") + 1);
+                int symb = new int();
+                if (extension == "efm")//mpg
+                {
+                    
+                    using (FileStream fs = new FileStream(file, FileMode.Open))
+                    {
+                        for (var i = 0; i < fs.Length; i++)
+                        {
+                            symb = fs.ReadByte();
+                            symb--;
+                            fs.Seek(-1, SeekOrigin.Current);
+                            fs.WriteByte((byte)symb);
+                        }
+                    }
+
+                    string[] fName = new string[2];
+                    fName = file.Split('.');
+                    File.Move(file, fName[0] + ".mpg");
+
+                }
+                else if(extension == "efo")
+                {
+                    using (FileStream fs = new FileStream(file, FileMode.Open))
+                    {
+                        for (var i = 0; i < countOfSymbol; i++)
+                        {
+                            symb = fs.ReadByte();
+                            symb--;
+                            fs.Seek(-1, SeekOrigin.Current);
+                            fs.WriteByte((byte)symb);
+                        }
+                    }
+                    string[] fName = new string[2];
+                    fName = file.Split('=');
+                    string[] extension = fName[1].Split('.');
+                    File.Move(file, fName[0] + "."+ extension[0]);
+                }
+                
+            }
+            textBoxInfo.Text = "Декодирование завершено.";
+        }
+       
+        //выбрать путь
+        private void buttonPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+
+            
+
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                textBoxPath.Text = folderBrowser.SelectedPath;
+                files = Directory.GetFiles(textBoxPath.Text);
+
+            foreach (var file in files)
+                textBoxInfo.Text += "\r\n" + file.ToString() + "\r\n";
+            }
         }
     }
 }
+
+
